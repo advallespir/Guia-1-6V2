df3a670d1d344e7dd500e69feabdd6f2bd717051
ADVallespir <advallespir@gmail.com>
2021-03-25 20:21:03 -0300

    Arreglo de Merge

--- Program.cs
+++ Program.cs
@@ -7,28 +7,25 @@ namespace Guia_1___6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Solo salis de aca si apretas control + shift + F");
+
             ConsoleKeyInfo tecla;
-            Console.TreatControlCAsInput = true;
 
-            Console.TreatControlCAsInput = true;
 
-            Console.WriteLine("Apreta Control + Shift + F para salir, sino mientras te quedas acá.");
             do
+            {
+                tecla = Console.ReadKey();
                 if ((tecla.Key == ConsoleKey.F) &&
                     ((tecla.Modifiers & ConsoleModifiers.Shift) != 0) &&
                         ((tecla.Modifiers & ConsoleModifiers.Control) != 0))
                 {
                     break;
-                    //test
-            {
-                tecla = Console.ReadKey();
-                
-            } while (true);
                 }
-                Console.WriteLine("{0} (character '{1}')", cki.Key, cki.KeyChar);
+                    //test
+           
+        
+              
+            } while (true);
 
-           } while (igualdad == false);
         }
     }
 }
